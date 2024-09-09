089bb1e83617764bc6b79ad9d41044a2ab5f7fea
xiehao <mysunshine2126@gmail.com>
2024-09-09 18:12:18 +0800

    feat(main): complete main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
-
+#include "include/memory/memory_pool.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    MemoryPool *mem_pool = new MemoryPool();
+
+    MemoryChunk *mem_chunk = mem_pool->new_chunk(78);
+
+    MemoryCell *cell1 = mem_chunk->malloc(8);
+    MemoryCell *cell2 = mem_chunk->malloc(16);
+    MemoryCell *cell3 = mem_chunk->malloc(32);
+    MemoryCell *cell4 = mem_chunk->malloc(32);
+
+    *((char *) cell3->ptr()) = 'a';
+
+    INFO_PRINT("%X========>%c\n", cell3, *((char*)cell3->ptr()));
+
+    mem_chunk->malloc(40);
+
+    PRINT("%X======>%c\n", cell3->ptr(), *((char*)cell3->ptr()));
+
+    mem_pool->print_chunks();
+    delete mem_pool;
+
     return 0;
+
 }
+
